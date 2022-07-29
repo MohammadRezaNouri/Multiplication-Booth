c29ff3f3e2505038873c4e4b7baf7652aabed6a2
MohammadRezaNouri <MohammadRezaNouri01@Gmail.Com>
2022-07-29 06:21:44 +0430

    Write the addBinary function
    This function adds two n-bit binary numbers together as a string.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,36 @@ using namespace std;
 #define f1c(val) cout << val << "\n";
 #define ret0 return 0;
 
+string addBinary(string x, string y)
+{
+    int c = 0;
+    string res = "";
+    for (int i = 0; i < x.size(); i++)
+        res += '0';
+    for (int i = x.size() - 1; i >= 0; i--)
+        if (x[i] == '0' && y[i] == '0' && c == 0)
+        {
+            res[i] = '0';
+            c = 0;
+        }
+        else if ((x[i] == '0' && y[i] == '0' && c == 1) || (x[i] == '0' && y[i] == '1' && c == 0) || x[i] == '1' && y[i] == '0' && c == 0)
+        {
+            res[i] = '1';
+            c = 0;
+        }
+        else if (x[i] == '0' && y[i] == '1' && c == 1 || (x[i] == '1' && y[i] == '0' && c == 1) || (x[i] == '1' && y[i] == '1' && c == 0))
+        {
+            res[i] = '0';
+            c = 1;
+        }
+        else if (x[i] == '1' && y[i] == '1' && c == 1)
+        {
+            res[i] = '1';
+            c = 1;
+        }
+    return res;
+}
+
 int main()
 {
     ret0;
